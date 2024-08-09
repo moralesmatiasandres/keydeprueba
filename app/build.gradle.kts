d1c7956446d72af7ce049acdd92e100c38a1d138
matias morales <moralesmatiasandres@gmail.com>
2024-08-09 19:48:11 -0300

    implementando cicd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.basicstatecodelab"
+        applicationId = "com.mosqi.basicstatecodelab"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
